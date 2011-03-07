54edf70ac0199156c99d4786b983138a66648934
Ori Livneh <ori.livneh@gmail.com>
2011-03-07 10:09:44 -0500

    First commit

---
+++ README.mk
@@ -0,0 +1,9 @@
+# psi.js: Pythonic Strings for JavaScript #
+<https://github.com/zelak/psi.js>
+
+psi.js extends JavaScript's strings with methods from the 
+[Python Standard Library](http://docs.python.org/dev/library/string.html).
+
+
+Copyright 2011, Ori Livneh <ori.livneh@gmail.com>
+Released under the MIT or GPL Version 2 licenses.
