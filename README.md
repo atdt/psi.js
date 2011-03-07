6c9baf3cac2aca9c40a652d888a1a7f0a4cc55be
Ori Livneh <ori.livneh@gmail.com>
2011-03-07 11:55:36 -0500

    boilerplate test page + some functions

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ psi.js extends JavaScript's strings with methods from the
 [Python Standard Library](http://docs.python.org/dev/library/string.html).
 
 
-Copyright 2011, Ori Livneh <ori.livneh@gmail.com>
+Copyright 2011, Ori Livneh <ori.livneh@gmail.com>  
 Released under the MIT or GPL Version 2 licenses.
---
+++ psi.js
@@ -0,0 +1,54 @@
+/* psi.js 
+ * Pythonic Strings for JavaScript
+ * https://github.com/zelak/psi.js
+ *
+ * psi.js is an implementation of the Python Standard Library's
+ * string methods in pure JavaScript.
+ *
+ * Copyright 2011, Ori Livneh
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+
+    // whitespace = ' \t\n\r\v\f';
+    // ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    // ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // ascii_letters = ascii_lowercase + ascii_uppercase;
+    // digits = '0123456789';
+    // hexdigits = digits + 'abcdef' + 'ABCDEF';
+    // octdigits = '01234567';
+    // punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    // printable = digits + ascii_letters + punctuation + whitespace
+
+py = String.prototype;
+
+py.startswith = function(str) {
+    // todo: take optional parameters
+    return this.substr(0 , str.length) === str;
+};
+
+py.splitlines = function(keepends) {
+    // todo: keep newlines if keepends is true
+    return this.split('\n');
+}
+
+py.capitalize = function() {
+    return;
+}
+
+py.upper = function() {
+    return this.toUpperCase();
+}
+
+py.isupper = function() {
+    return this == this.toUpperCase();
+}
+
+py.capitalize = function() {
+    return this.substr(0, 1).toUpperCase() + this.substr(1).toLowerCase();
+}
+
+py.endswith = function(str) {
+    // this will break in ie
+    return this.substr(0 - str.length) === str;
+}
+
